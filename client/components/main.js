0a75d3973260a4f19406dc3f193de68a0a5320ea
Junjie Chen <junjie.chen18@gmail.com>
2017-09-13 10:43:31 -0400

    completed Navbar

--- client/components/main.js
+++ client/components/main.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
 import {logout} from '../store'
+import Navbar from './Navbar'
 
 /**
  * COMPONENT
@@ -14,26 +15,27 @@ const Main = (props) => {
   const {children, handleClick, isLoggedIn} = props
 
   return (
-    <div>
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to='/home'>Home</Link>
-              <a href='#' onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to='/login'>Login</Link>
-              <Link to='/signup'>Sign Up</Link>
-            </div>
-        }
-      </nav>
-      <hr />
-      {children}
-    </div>
+    // <div>
+    //   <h1>BOILERMAKER</h1>
+    //   <nav>
+    //     {
+    //       isLoggedIn
+    //         ? <div>
+    //           {/* The navbar will show these links after you log in */}
+    //           <Link to='/home'>Home</Link>
+    //           <a href='#' onClick={handleClick}>Logout</a>
+    //         </div>
+    //         : <div>
+    //           {/* The navbar will show these links before you log in */}
+    //           <Link to='/login'>Login</Link>
+    //           <Link to='/signup'>Sign Up</Link>
+    //         </div>
+    //     }
+    //   </nav>
+    //   <hr />
+    //   {children}
+    // </div>
+    <Navbar />
   )
 }
 
