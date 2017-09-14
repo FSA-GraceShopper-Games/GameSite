57e15024a37467c3dbcde5d71ffda8b66089d4ce
Junjie Chen <junjie.chen18@gmail.com>
2017-09-14 12:13:39 -0400

    updated Navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -38,7 +38,7 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <a className="navbar-brand" href="#">Ninja</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
