f94d3e25a669727802f371edbe582759fc08c632
Leslie Chiu <leslie@Leslies-MacBook-Pro.local>
2017-09-18 12:40:33 -0400

    testing order routes

--- server/api/order.spec.js
+++ server/api/order.spec.js
@@ -11,38 +11,16 @@ const Product = db.model('product')
 
 describe('Order routes', () =>{
     beforeEach(() => {
-        db.sync({force: true})
-
-        // User.create({
-        //     name: 'Les',
-        //     email: 'les@mail.com',
-        //     password: '123',
-        //     introduction: 'random string',
-        //     salt: 'hash',
-        //     googleId: 'abc' 
-        // })
-
-        Order.create({
+        return db.sync({force: true})
+          .then(() => {
+            return Order.create({
                 progress: 'in-progress', 
                 totalPrice: 30,
-                date: new Date(),
-                // userId: 1            
-        })
-
-        // LineItem.create({
-        //     orderId: 1,
-        //     userId: 1
-        // })
-
-        // Product.create({
-        //     name: 'Destiny',
-        //     description: 'super cool game and stuff yo!',
-        //     image: "https://upload.wikimedia.org/wikipedia/en/b/be/Destiny_box_art.png",
-        //     price: 49
-        // })
-
-
-    })
+                date: new Date()
+            })
+          })
+    }) 
+    
 
     describe('/api/order', ()=>{
 
@@ -58,13 +36,13 @@ describe('Order routes', () =>{
     })
 
     //not sure how to test. getting manny errors...
-    // describe('/api/order/user/:id', () =>{
-    //     xit('GET /api/order/user/:id', () =>{
-    //         return request(app)
-    //           .get('/api/order/user/1')
-    //           .expect(200)
-    //     })
-    // })
+    describe('/api/order/user/:id', () =>{
+        it('GET /api/order/user/:id', () =>{
+            return request(app)
+              .get('/api/order/user/1')
+              .expect(200)
+        })
+    })
 
     describe('/api/order/:id', ()=>{
         it('GET /api/order/:id', ()=>{
@@ -72,31 +50,5 @@ describe('Order routes', () =>{
               .get('/api/order/1')
               .expect(200)
         })
-
     })
-
-
 })
-
-        // beforeEach(()=>{
-        //     return Order.create({
-        //         progress: 'in-progress', 
-        //         totalPrice: 30,
-        //         date: new Date(),
-        //         // userId: 1
-        //     })
-        // })
-
-
-
-        //     // Not sure how to test ??
-        // xit('GET orders by user Id', () => {
-        //     return request(app)
-        //       .get('/api/user/1')
-        // })
-
-        // xit('GET orders by id', ()=>{
-        //     return request(app)
-        //       .get('/api/order/:id')
-        //       .expect(200)
-        // })
